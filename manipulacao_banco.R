30cfaf7f01650950d8996812373c1aa7ef1f0243
claudioalvesmonteiro <claudiomonteiro@protonmail.com>
2018-01-12 10:50:00 -0300

    commit 0

---
+++ manipulacao_banco.R
@@ -0,0 +1,33 @@
+#========================================#
+# VIOLENCIA E OPINIAO PUBLICA            #    
+#========================================#
+# Recife - Pernambuco - Brasil           #
+# Janeiro 2018                           #    
+#----------------------------------------#
+# Claudio A. Monteiro                    #
+# Gabriella Fidelis                      #
+# Leticia Machado                        #
+#----------------------------------------#
+# Any question contact the developers    #
+# #UseFreeSoftware                       #
+#----------------------------------------#
+
+# instalar pacotes necessarios
+# install.packages(c("readxl"))
+
+# carregar pacote
+library(readxl)
+
+# ler bancos de dados
+LapopBrazil_2006 <- read_excel("Dados/LapopBrazil_2006.xlsx")
+LapopBrazil_2008 <- read_excel("Dados/LapopBrazil_2008.xlsx")
+LapopBrazil_2010 <- read_excel("Dados/LapopBrazil_2010.xlsx")
+LapopBrazil_2012 <- read_excel("Dados/LapopBrazil_2012.xlsx")
+LapopBrazil_2014 <- read_excel("Dados/LapopBrazil_2014.xlsx")
+
+
+
+
+
+
+
